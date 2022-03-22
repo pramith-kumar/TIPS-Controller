d6c6a130d4ebb806015eb11bf59a5ad877b6bc94
pramith <pramith2009@gmail.com>
2022-03-22 13:02:03 +0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -5,9 +5,11 @@
 #**************************************************************************************************
 import os
 import socket
+import threading
 import sys
 from gpiozero import Button, LED
 import time
+
 
 Device_Id = 21
 
@@ -16,10 +18,8 @@ SERVER_PORT = 20108
 
 CLIENT_IP = "192.168.100.250"				# DO NOT FORGET TO SET RASPBERRY PI
 CLIENT_PORT = 20107
-sock = socket.socket(socket.AF_INET,
-socket.SOCK_DGRAM) 							# UDP Receive
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 							# UDP Receive
 sock.bind((CLIENT_IP, CLIENT_PORT))
-
 
 V_Loop = Button(1)							#	GPIO1
 N_Loop = Button(2)							#	GPIO2
@@ -38,35 +38,60 @@ Barrier_Arm_Broken_Prev_Status = 0
 Barrier_Up_Prev_Status = 0
 Barrier_Down_Prev_Status = 0
 
-while True:
-# GPIO input check in loop
-	if (( V_Loop.is_pressed ) and ( V_Loop_Prev_Status == 0 )) :
-		V_Loop_Prev_Status = 1
-		sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-		sock.sendto(Device_Id + "101", (SERVER_IP,SERVER_PORT))
+class Receive(object):
+	def  __init__(self):
+		self.stuff = 'Receive'
+	def receive(self):
+		while True:
+			data_new, addr = sock.recvfrom(1024) 
+			data = data_new
+			print("received command: %s" % data)
 
-	if (( N_Loop.is_pressed ) and ( N_Loop_Prev_Status == 0 )):
-		N_Loop_Prev_Status = 1
+			if ( data == "45" ):
+				Open_Sp.on()
+				time.sleep(0.1)
+				Open_Sp.off()
 
-	if (( Barrier_Arm_Broken.is_pressed ) and ( Barrier_Arm_Broken_Prev_Status == 0 )):
-		Barrier_Down_Prev_Status = 1
+			if ( data == "46" ):
+				Close.on()
+				time.sleep(0.1)
+				Close.off()
 
-	if (( Barrier_Up.is_pressed ) and ( Barrier_Up_Prev_Status == 0 )):
-		Barrier_Up_Prev_Status = 1
+			if ( data == "81" ):
+				os.system("sudo reboot")
 
-	if ((Barrier_Down.is_pressed) and ( Barrier_Down_Prev_Status == 0 )):
-		Barrier_Down_Prev_Status = 1
+class Send(object):
+	def __init__(self):
+		self.stuff = 'Send'
+	def send(self):
+		while True:		# GPIO input check in loop
+			if ((V_Loop.is_pressed) and (V_Loop_Prev_Status == 0)):
+				V_Loop_Prev_Status = 1
+				sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+				sock.sendto(Device_Id + "101", (SERVER_IP, SERVER_PORT))
 
-# GPIO out - UDP Receive starts here
-	data, addr = sock.recvfrom(1024) 
-	print("received command: %s" % data)
-	if ( data == "45" ):
-		Open_St.on()
-		time.sleep(0.1)
-		Open_St.off()
-	if ( data == "46" ):
-		Close.on()
-		time.sleep(0.1)
-		Close.off()
-	if ( data == "81" ):
-		os.system("sudo reboot")
+			if ((N_Loop.is_pressed) and (N_Loop_Prev_Status == 0)):
+				N_Loop_Prev_Status = 1
+
+			if ((Barrier_Arm_Broken.is_pressed) and (Barrier_Arm_Broken_Prev_Status == 0)):
+				Barrier_Down_Prev_Status = 1
+
+			#if ((Barrier_Up.is_pressed) and (Barrier_Up_Prev_Status == 0)):
+				#Barrier_Up_Prev_Status = 1
+
+			if ((Barrier_Down.is_pressed) and (Barrier_Down_Prev_Status == 0)):
+				Barrier_Down_Prev_Status = 1
+
+			print("I am OK")
+			time.sleep(1)
+
+def main():
+	r = Receive()
+	s = Send()
+	t1 = threading.Thread(target = r.receive)
+	t2 = threading.Thread(target = s.send)
+	t1.start()
+	t2.start()
+
+if __name__ == '__main__':
+	main()
